759c7b1e5fe3e06564f02ed5810741cb24ccd4c2
Sunil G <gsunil99910@gmail.com>
2023-01-26 14:23:32 +0530

    completed triangles

---
+++ area.js
@@ -0,0 +1,23 @@
+var base = document.querySelector("#base");
+var height = document.querySelector("#height");
+var btn = document.querySelector("#btn-area");
+var output = document.querySelector("#area-output");
+
+btn.addEventListener("click", handleArea)
+
+function validateNumber(str) {
+    return !isNaN(str) && str.trim().length > 0;
+  }
+
+  function nonZero(number) {
+    return Number(number) > 0;
+}
+
+function handleArea() {
+    output.style.display = "block";
+    if(validateNumber(base.value) && validateNumber(height.value) && nonZero(base.value) && nonZero(height.value)) {
+        output.innerText = "Area is " + 0.5*(Number(base.value) + Number(height.value))+" cm";
+    } else {
+        output.innerText = "INVALID";
+    }
+}
---
+++ hypotenuse.js
@@ -0,0 +1,24 @@
+
+var a = document.querySelector("#a");
+var b = document.querySelector("#b");
+var btn = document.querySelector("#btn-hypotenuse");
+var output = document.querySelector("#hypo-output");
+
+btn.addEventListener("click", handleHypo)
+
+function validateNumber(str) {
+    return !isNaN(str) && str.trim().length > 0;
+  }
+
+  function nonZero(number) {
+    return Number(number) > 0;
+}
+
+function handleHypo() {
+    output.style.display = "block";
+    if(validateNumber(a.value) && validateNumber(b.value) && nonZero(a.value) && nonZero(b.value)) {
+        output.innerText = "Hypotenuse is " + Math.round(Math.sqrt(Number(a.value)*Number(a.value) + Number(b.value)*Number(b.value))) +" cm";
+    } else {
+        output.innerText = "INVALID";
+    }
+}
